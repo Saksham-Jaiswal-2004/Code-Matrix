85e17b74db3d278ebf67b9f0e077b21c05925589
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-06-04 21:43:35 +0530

    About and Services Page

--- app/components/CertStrip.js
+++ app/components/CertStrip.js
@@ -6,8 +6,8 @@ const CertStrip = () => {
       <h1 className='title3'>Certified By</h1>
 
       <div className='w-full h-fit flex justify-center items-center gap-[40px]'>
-        <img src="/images/CertLogo1.jpg" alt="" className='w-[100px] h-auto saturate-0 opacity-70'/>
-        <img src="/images/CertLogo2.jpg" alt="" className='w-[100px] h-auto saturate-0 opacity-70'/>
+        <img src="/images/CertLogo1.JPG" alt="" className='w-[100px] h-auto saturate-0 opacity-70'/>
+        <img src="/images/CertLogo2.JPG" alt="" className='w-[100px] h-auto saturate-0 opacity-70'/>
         <img src="/images/CertLogo3.png" alt="" className='w-[100px] h-auto saturate-0 opacity-70'/>
       </div>
     </div>
